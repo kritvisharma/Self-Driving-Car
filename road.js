e3183178902cd9afac5fc996e05c984082002b0b
Kritvi Sharma <kritvisharma0912@gmail.com>
2023-07-03 16:42:24 +0400

    road updated

--- road.js
+++ road.js
@@ -12,24 +12,25 @@ class Road {
         this.bottom=infinity;
     }
 
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width/this.lanecount;
+        return this.left+laneWidth/2 + Math.min(laneIndex,this.lanecount-1)* laneWidth;
+    }
+
     draw(ctx){
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        for(let i=0; i<=this.lanecount;i++){
-            const x = lerp(
-                this.left,
-                this.right,
-                i/this.lanecount
-            );
+        for(let i=0; i<=this.lanecount; i++){
+
+            const x = lerp(this.left, this.right, i/this.lanecount);
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
+
         }
     }
 }
 
-function lerp(A,B,t){
-    return A+(B-A)*t;
-}
+
